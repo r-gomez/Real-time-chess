d6dd0b49edd8ead13a5f263a6821485752ac4210
r-gomez <lianekeo@gmail.com>
2017-05-05 17:16:01 -0500

    Add files via upload

--- public/default.js
+++ public/default.js
@@ -5,15 +5,49 @@ var initGame = function() {
 		draggable: true,
 		position: 'start',
 		onDrop: handleMove,
+		onDragStart: otherSide
 	};
+
 	
 	board = new ChessBoard('gameBoard', cfg);
 	game = new Chess();
 };
 
-var handleMove = function(source, target) {
-	var move = game.move({from: source, to: target});
+
+//makes sure that you cant mess with the other side
+var otherSide = function(source, piece, position, orientation){ 
+	if(game.in_checkmate() === true || game.in_draw() === true || piece.search(/^b/)!== -1){
+	return false;
+	}
+};
+/*
+turn allows you to see whose turn it is after you make a move
+b  default.js:28:2
+Valid move!  default.js:34:3
+w  default.js:28:2
+Valid move!
+*/
+var simpleComp = function() {
+	var possibleMoves = game.moves();
+	if(possibleMoves.length === 0){
+		console.log('Game is over');
+		return;
+	}
+	else{
+		var randIndex = Math.floor(Math.random() * possibleMoves.length);
+		game.move(possibleMoves[randIndex]);
+		board.position(game.fen());
+		window.setTimeout(simpleComp, 1000);
+	}
 	
+};
+
+
+
+var handleMove = function(source, target) {
+	console.log(game.turn());
+	var move = game.move({from: source, to: target});
+	/*
 	if(move === null){
 		console.log('Invalid move! Snapping back.');
 		return 'snapback';
@@ -22,6 +56,8 @@ var handleMove = function(source, target) {
 		console.log('Valid move!');
 		socket.emit('move', move); //hey someone made a move 'move' and move is just what actual move it made
 	}
+	*/
+	window.setTimeout(simpleComp, 1000);
 }
 //player 2 receives the message so the game moves according to that msg
 socket.on('move', function(msg) {
@@ -30,6 +66,7 @@ socket.on('move', function(msg) {
 });
 
 initGame();
+
 //they emit a message to the server
 //broadcast sends out that message back to both players
 //we use web sockets because its really fast and we can emmmit quickly
